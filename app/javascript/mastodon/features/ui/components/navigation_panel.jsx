dc8987dbc1aacdeef01a26cf9f85bfe9f97b60aa
nesosuke <neso@neso.tech>
2024-08-05 22:15:13 +0900

    megre忘れ解消

--- app/javascript/mastodon/features/ui/components/navigation_panel.jsx
+++ app/javascript/mastodon/features/ui/components/navigation_panel.jsx
@@ -127,17 +127,6 @@ class NavigationPanel extends Component {
         </a>
       </div>);
 
-    let banner = undefined;
-
-    if(transientSingleColumn)
-      banner = (<div className='switch-to-advanced'>
-        {intl.formatMessage(messages.openedInClassicInterface)}
-        {" "}
-        <a href={`/deck${location.pathname}`} className='switch-to-advanced__toggle'>
-          {intl.formatMessage(messages.advancedInterface)}
-        </a>
-      </div>);
-
     return (
       <div className='navigation-panel'>
         <div className='navigation-panel__logo'>
